5b887ef0b0c0f32d279fe83c21a3c8a7247ab694
Matheus-Almeida-Ribeiro <matheus.a.ribeiro0505@gmail.com>
2026-05-21 15:08:07 +0000

    configura projeto.

---
+++ devops-biblioteca/src/main/java/com/matheus/devops_biblioteca/DevopsBibliotecaApplication.java
@@ -0,0 +1,13 @@
+package com.matheus.devops_biblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsBibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsBibliotecaApplication.class, args);
+	}
+
+}
